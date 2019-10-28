2dc11d31239e8fadfc7d7b998e7115be00b2c68a
Jack <js3367@edu.turku.fi>
2019-10-28 14:18:40 +0200

    hello

--- calculator/main.js
+++ calculator/main.js
@@ -5,6 +5,19 @@ const prompt = require('prompt-sync')()
 const run = () => {
 
     let number1 = prompt('Number1:')
+    let operator = prompt ('input operator')
+    let number2 = prompt('Number2:')
+    number1 = parseInt(number1)
+    number2 = parseInt(number2)
+    if (operator == '+' ){
+        console.log(number1 + number2)
+    }else if (operator == '-' ){
+        console.log(number1 - number2)
+    }else if (operator == '*' ){
+        console.log(number1 * number2)
+    }else if (operator == '/' ){
+        console.log(number1 / number2)
+    }else {console.log('Nuudeli')}
 
 }
 
